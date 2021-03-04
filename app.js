c0bd934c9236d879c42fc505ce47b9302be782ec
Jaidev Das <jaidevkr2@gmail.com>
2021-03-04 13:43:07 +0530

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import "express-async-errors";
 import dotenv from "dotenv";
@@ -8,6 +9,8 @@ import authRoutes from "./routes/auth.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
+
+app.use(cors());
 
 dotenv.config();
 app.use(express.json());
